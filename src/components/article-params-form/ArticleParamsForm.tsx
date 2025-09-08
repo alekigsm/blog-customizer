8a3fbd8e1209ce6d5a0f4092c11d0a77da5592ef
alekigsm <Igr0k@yandex.ru>
2025-09-08 21:51:51 +0300

    fix all bag

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,7 +1,7 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 import styles from './ArticleParamsForm.module.scss';
-import { useRef, useState } from 'react';
+import { SyntheticEvent, useCallback, useRef, useState } from 'react';
 import { Text } from 'src/ui/text';
 import { clsx } from 'clsx';
 import { Select } from 'src/ui/select';
@@ -13,39 +13,25 @@ import {
 	fontColors,
 	fontFamilyOptions,
 	fontSizeOptions,
-	OptionType,
 } from 'src/constants/articleProps';
 import { RadioGroup } from 'src/ui/radio-group';
 import { useOutsideClickClose } from 'src/ui/select/hooks/useOutsideClickClose';
-
+import { Separator } from 'src/ui/separator';
 type ArticleParamsFormProps = {
 	onStateChange: (newState: ArticleStateType) => void;
 	state: ArticleStateType;
 };
-
 export const ArticleParamsForm = (props: ArticleParamsFormProps) => {
-	const state = props.state;
 	const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
-	const [classNameContainer, setclassNameContainer] = useState<string>(
-		styles.container
-	);
+	//поясни
+	const classNameContainer = clsx(styles.container, {
+		[styles.container_open]: isMenuOpen,
+	});
 	function onClickOpen() {
-		setIsMenuOpen((open) => {
-			open = !open;
-			if (open) {
-				setclassNameContainer(clsx(styles.container, styles.container_open));
-			} else {
-				setclassNameContainer(clsx(styles.container));
-			}
-			return open;
-		});
+		setIsMenuOpen((prev) => !prev);
 	}
 	function close() {
-		setIsMenuOpen((open) => {
-			open = false;
-			setclassNameContainer(clsx(styles.container));
-			return open;
-		});
+		setIsMenuOpen(false);
 	}
 	const rootRef = useRef<HTMLDivElement>(null);
 	useOutsideClickClose({
@@ -53,55 +39,8 @@ export const ArticleParamsForm = (props: ArticleParamsFormProps) => {
 		rootRef,
 		onChange: close,
 	});
-	const [stateFont, setStateFont] = useState(state.fontFamilyOption);
-	const [stateFontColor, setStateFontColor] = useState(state.fontColor);
-	const [stateBackGroundColor, setStateBackGroundColor] = useState(
-		state.backgroundColor
-	);
-	const [stateContentWidth, setStateContentWidth] = useState(
-		state.contentWidth
-	);
-	const [stateFontSize, setStateFontSize] = useState(state.fontSizeOption);
-	function onChangeFontFamily(selected: OptionType): void {
-		setStateFont(selected);
-	}
-
-	function onChangeFontColor(selected: OptionType): void {
-		setStateFontColor(selected);
-	}
-
-	function onChangeBackGroundColor(selected: OptionType): void {
-		setStateBackGroundColor(selected);
-	}
-
-	function onChangeContentWidth(selected: OptionType): void {
-		setStateContentWidth(selected);
-	}
-
-	function onChangeFontSize(selected: OptionType): void {
-		setStateFontSize(selected);
-	}
-
-	function onCliCkReset() {
-		setStateFont(defaultArticleState.fontFamilyOption);
-		setStateFontColor(defaultArticleState.fontColor);
-		setStateBackGroundColor(defaultArticleState.backgroundColor);
-		setStateContentWidth(defaultArticleState.contentWidth);
-		setStateFontSize(defaultArticleState.fontSizeOption);
-		props.onStateChange(defaultArticleState);
-	}
-	function onClickApply() {
-		props.onStateChange({
-			fontFamilyOption: stateFont,
-			fontColor: stateFontColor,
-			backgroundColor: stateBackGroundColor,
-			contentWidth: stateContentWidth,
-			fontSizeOption: stateFontSize,
-		});
-		//onClickOpen();
-	}
-	/* // Универсальная фабрика обработчиков изменения поля состояния
-	const [formState, setFormState] = useState(state);
+	// Универсальная фабрика обработчиков изменения поля состояния
+	const [formState, setFormState] = useState(props.state);
 	const createChangeHandler = useCallback(
 		<K extends keyof ArticleStateType>(key: K) =>
 			(selectedOption: ArticleStateType[K]) => {
@@ -111,67 +50,63 @@ export const ArticleParamsForm = (props: ArticleParamsFormProps) => {
 				}));
 			},
 		[]
-	); */
-
-	/*
-	Мы нажимаем на кнопке мыши левую кнопку, срабатывает обработчик события onclick, который в свою очеред вызывает нашу функцию onClickOpen 
-	в котором мы вызываем функцию setIsOpen т.к. вызывается функция setIsOpen в котором параметр функция то эта функция добавляется в очередь
-	после мы проверяем текущее значение isopen меняем классы и выходим из функции, после выхода из обработчика react перересовывает компонент и видимт что в очереди есть функции для изменения значения 
-	и вызывает их меняя значения на новые
-	*/
+	);
+	function onClickReset() {
+		setFormState(defaultArticleState);
+		props.onStateChange(defaultArticleState);
+	}
+	const handleSubmit = (e: SyntheticEvent) => {
+		e.preventDefault();
+		props.onStateChange(formState);
+	};
 	return (
 		<>
 			<ArrowButton isOpen={isMenuOpen} onClick={onClickOpen} />
 			<aside className={classNameContainer} ref={rootRef}>
-				<form className={styles.form}>
+				<form className={styles.form} onSubmit={handleSubmit}>
 					<Text as='h1' size={45} weight={800} uppercase>
 						Задайте параметры
 					</Text>
 					<Select
-						selected={stateFont}
+						selected={formState.fontFamilyOption}
 						options={fontFamilyOptions}
 						title='Шрифт'
-						onChange={onChangeFontFamily}
-						//	onChange={}
+						onChange={createChangeHandler('fontFamilyOption')}
 					/>
 					<RadioGroup
-						selected={stateFontSize}
+						selected={formState.fontSizeOption}
 						options={fontSizeOptions}
 						title='Размер Шрифта'
-						onChange={onChangeFontSize}
+						onChange={createChangeHandler('fontSizeOption')}
 						name='radio'
 					/>
 					<Select
-						selected={stateFontColor}
+						selected={formState.fontColor}
 						options={fontColors}
 						title='Цвет шрифта'
-						onChange={onChangeFontColor}
+						onChange={createChangeHandler('fontColor')}
 					/>
+					<Separator />
 					<Select
-						selected={stateBackGroundColor}
+						selected={formState.backgroundColor}
 						options={backgroundColors}
 						title='Цвет фона'
-						onChange={onChangeBackGroundColor}
+						onChange={createChangeHandler('backgroundColor')}
 					/>
 					<Select
-						selected={stateContentWidth}
+						selected={formState.contentWidth}
 						options={contentWidthArr}
 						title='Ширина контента'
-						onChange={onChangeContentWidth}
+						onChange={createChangeHandler('contentWidth')}
 					/>
 					<div className={styles.bottomContainer}>
 						<Button
 							title='Сбросить'
 							htmlType='reset'
 							type='clear'
-							onClick={onCliCkReset}
+							onClick={onClickReset}
 						/>
-						<Button
-							title='Применить'
-							htmlType='button'
-							type='apply'
-							onClick={onClickApply}
-						/>
+						<Button title='Применить' htmlType='submit' type='apply' />
 					</div>
 				</form>
 			</aside>
